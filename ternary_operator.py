8812e1bb1ebe95abd9afc64194f7814d4e0d370e
shalulu94 <shahrulnasir290@hotmail.com>
2024-04-06 10:51:27 +1100

    list and tuples

---
+++ ternary_operator.py
@@ -0,0 +1,11 @@
+is_raining = False
+
+# if is_raining:
+#     print("I need an umbrella")
+# else:
+#     print("I don't need an umbrella")
+
+#  Ternary operator
+# <statement to execute when true> if <condition> else <statement to execute when false>
+
+print("I need an umbrella") if is_raining else print("I don't need an umbrella")
